820b70cb2cd3cafb59b5e14d7fb005b949cb3b0f
petrmareltuev <66667579+petrmareltuev@users.noreply.github.com>
2021-02-04 14:27:08 +0300

    Update task2.py

--- task2/task2.py
+++ task2/task2.py
@@ -4,20 +4,10 @@ import time
 import locale
 import ctypes
 
-def main():
+def main(s1, s2):
     rus = check_if_russian_locale()
     path = get_desktop_path()
     os.startfile("notepad.exe")
-
-    s1 = """    Варкалось. Хливкие шорьки
-    Пырялись по наве,
-    И хрюкотали зелюки,
-    Как мюмзики в мове."""
-
-    s2 = """\n    О, бойся Бармаглота, сын!
-    Он так свирлеп и дик!
-    А в глу́ше ры́мит исполин —
-    Злопастный Брандашмыг!"""
 
     create_barmaglot_file(s1, path, rus)
     create_new(s2)
@@ -25,7 +15,6 @@ def main():
 def check_if_russian_locale():
     windll = ctypes.windll.kernel32
     lang = locale.windows_locale[ windll.GetUserDefaultUILanguage() ]
-
     return (lang == 'ru_RU'):
 
 def get_desktop_path():
@@ -35,9 +24,12 @@ def get_desktop_path():
     return path
 
 def create_barmaglot_file(s1, path, rus):
+    # open save dialog
     time.sleep(2)
     keyboard.write(s1)
     keyboard.send("ctrl+shift+s")
+    
+    # write desktop path
     time.sleep(2)
     keyboard.send("f4")
     time.sleep(1)
@@ -45,21 +37,23 @@ def create_barmaglot_file(s1, path, rus):
     keyboard.write(path)
     keyboard.send("enter")
     time.sleep(1)
+    
+    # go to filename input
     keyboard.send("tab")
     time.sleep(1)
     keyboard.send("esc")
     time.sleep(1)
-
-    if (rus == False):
-        keyboard.send("alt+n")
-    else:
+    if rus:
         try:
             keyboard.send("alt+и")
         except ValueError:
             keyboard.send("alt+shift")
             time.sleep(1)
             keyboard.send("alt+b")
-
+    else:
+        keyboard.send("alt+n")
+        
+    # input filename and save 
     time.sleep(1)
     keyboard.write("barmaglot_file.txt")
     time.sleep(2)
@@ -76,4 +70,13 @@ def create_new(s2):
     keyboard.send("alt+f4")
 
 if __name__ == "__main__":
-    main()
+    s1 = """    Варкалось. Хливкие шорьки
+    Пырялись по наве,
+    И хрюкотали зелюки,
+    Как мюмзики в мове."""
+
+    s2 = """\n    О, бойся Бармаглота, сын!
+    Он так свирлеп и дик!
+    А в глу́ше ры́мит исполин —
+    Злопастный Брандашмыг!"""
+    main(s1, s2)
