c1c1301e52c94644693ab198cf76ca50c343f5cf
petrmareltuev <66667579+petrmareltuev@users.noreply.github.com>
2021-02-04 15:15:37 +0300

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -17,16 +17,16 @@ def main():
 		return
 
 	try:
-		cities_temp = parse_cities_temp(cities_data)
-		save_cities(cities_temp)
+		cities_temperatures = parse_cities_temp(cities_data)
+		save_cities(cities_temperatures)
 	except WebDriverException as e:
 		print(f"Проблема драйвера Google Chrome.\n{str(e)}")
 		return
 
-def get_temperature(driver, city, temp_type):
+def get_temperature(driver, city, temperature_type):
 	search_element = driver.find_element_by_name("q")
 	search_element.clear()
-	search_query = f"{city} temperature {temp_type}"
+	search_query = f"{city} temperature {temperature_type}"
 	search_element.send_keys(search_query)
 	search_element.send_keys(Keys.ENTER)
 	temperature_element = WebDriverWait(driver, 10).until(
@@ -40,22 +40,22 @@ def parse_cities_temp(cities_data):
 	driver.get("https://www.google.com/")
 	cities_temperatures = []
 	for city in cities_data['City']:
-		tempC = get_temperature(driver, city, "celsius")
-		tempF = get_temperature(driver, city, "fahrenheit")
-		cities_temperatures.append([tempC, tempF])
+		temperature_celsius = get_temperature(driver, city, "celsius")
+		temperature_fahrenheit = get_temperature(driver, city, "fahrenheit")
+		cities_temperatures.append([temperature_celsius, temperature_fahrenheit])
 	driver.close()
 	return cities_temperatures
 
-def save_cities(cities_temp):
+def save_cities(cities_temperatures):
 	current_time = datetime.datetime.now().strftime("%d.%m.%Y_%H.%M")
 	new_file_name = f"cities_{current_time}.xlsx"
 
 	wb = openpyxl.load_workbook('cities.xlsx')
 	sheet = wb[wb.sheetnames[0]]
 
-	for id, [tempC, tempF] in enumerate(cities_temp, start = 2):
-		sheet[f"B{id}"] = tempC
-		sheet[f"C{id}"] = tempF
+	for id, [temperature_celsius, temperature_fahrenheit] in enumerate(cities_temperatures, start = 2):
+		sheet[f"B{id}"] = temperature_celsius
+		sheet[f"C{id}"] = temperature_fahrenheit
 
 	print(new_file_name)
 	wb.save(new_file_name)
