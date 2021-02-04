3992c3f684f70fcfab45745b6126c54d8b6a36f1
mareltuevpetr <mareltuev.petr@rambler.ru>
2021-02-04 11:47:35 +0300

    english path hanling

--- task2/task2.py
+++ task2/task2.py
@@ -3,6 +3,13 @@ import os
 import time
 
 os.startfile("notepad.exe")
+english = False
+
+path = f"{os.environ['USERPROFILE']}\\Desktop"
+if (os.path.exists(path)):
+    english = True
+else:
+    path = f"{os.environ['USERPROFILE']}\\Рабочий стол"
 
 s1 = """Варкалось. Хливкие шорьки
 Пырялись по наве,
@@ -16,18 +23,34 @@ s2 = """\nО, бойся Бармаглота, сын!
 
 time.sleep(2)
 keyboard.write(s1)
-keyboard.send("ctrl+s")
-keyboard.write("barmaglot_file.txt")
+keyboard.send("ctrl+shift+s")
+time.sleep(2)
 keyboard.send("f4")
+time.sleep(1)
+keyboard.send("ctrl+a")
+keyboard.write(path)
+keyboard.send("enter")
+time.sleep(1)
+keyboard.send("tab")
+time.sleep(1)
+keyboard.send("esc")
+time.sleep(1)
+if (english == True):
+    keyboard.send("alt + n")
+else:
+    try:
+        keyboard.send("alt+и")
+    except ValueError:
+        keyboard.send("alt+shift")
+        keyboard.send("alt+и")
 
-for i in range(40): 
-  keyboard.send("backspace")
-keyboard.write("Desktop")
-keyboard.send("enter")
-
+time.sleep(2)
+keyboard.write("barmaglot_file.txt")
+time.sleep(2)
 keyboard.send("enter")
 keyboard.send("enter")
 keyboard.send("enter")
+time.sleep(2)
 
 keyboard.write(s2)
 keyboard.send("ctrl+shift+s")
